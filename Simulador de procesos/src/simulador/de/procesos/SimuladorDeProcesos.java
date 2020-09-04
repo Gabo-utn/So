ddc56a71f76306a984fb12951fa1594c0c488b25
Gabo-utn <64754786+Gabo-utn@users.noreply.github.com>
2020-09-03 20:22:30 -0500

    Simulador

---
+++ Simulador de procesos/src/simulador/de/procesos/SimuladorDeProcesos.java
@@ -0,0 +1,12 @@
+package simulador.de.procesos;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class SimuladorDeProcesos {
+
+    public static void main(String[] args) {
+        Ventana ventana = new Ventana();
+        ventana.setVisible(true);      
+    } 
+}
